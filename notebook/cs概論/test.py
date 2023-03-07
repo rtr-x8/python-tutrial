5b9d0dbcbc8983166b5db0181a77a4e55235cb47
rtr-x8 <ryu.2.24@hotmail.co.jp>
2023-03-07 22:00:33 +0900

    4

--- notebook/cs概論/test.py
+++ notebook/cs概論/test.py
@@ -1,1 +1,32 @@
-print("hello")
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(294/25.4, 210/25.4))
+ax = fig.add_axes([0.10, 0.10, 0.80, 0.80], projection=ccrs.PlateCarree())
+# ---
+# 別の図法で描きたい場合は例えば以下のようにする。
+# たくさんの図法が用意されています。
+# ax = fig.add_axes([0.10, 0.10, 0.80, 0.80], projection=ccrs.Mercator())
+# ax = fig.add_axes([0.10, 0.10, 0.80, 0.80], projection=ccrs.Robinson())
+# ax = fig.add_axes([0.10, 0.10, 0.80, 0.80], projection=ccrs.LambertCylindrical())
+# ax = fig.add_axes([0.10, 0.10, 0.80, 0.80], projection=ccrs.LambertConformal())
+
+ax.set_extent([120, 160, 20, 50], crs=ccrs.PlateCarree())
+ax.add_feature(cfeature.LAND)
+ax.add_feature(cfeature.OCEAN)
+ax.add_feature(cfeature.COASTLINE)
+ax.add_feature(cfeature.BORDERS, linestyle=':')
+ax.add_feature(cfeature.LAKES)
+ax.add_feature(cfeature.RIVERS)
+
+# --- 通天閣の位置
+ax.scatter(135+30/60+22.77/60/60, 34.0+39/60 +
+           9.13/60/60, transform=ccrs.PlateCarree())
+
+# --- 緯線と経線
+ax.gridlines(linestyle='dashed', color='gray',
+             linewidth=0.5,  draw_labels=True)
+
+plt.savefig('map-japan.png')
+
